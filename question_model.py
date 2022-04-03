369c8f020ac1f29510a08c2931ddfb655875ef89
dilshod996 <59019957+dilshod996@users.noreply.github.com>
2022-04-03 20:31:35 +0900

    Delete question_model.py

--- question_model.py
+++
@@ -1,8 +0,0 @@
-class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-
-
-new_q = Question("What is gold color? ", "yellow")
-print(new_q.text)
